9e85352cfd385a621d2056aa44b460f6297b8f58
Mark Meijer <engineermarkpro@gmail.com>
2020-11-27 21:04:57 +0100

    project creation

---
+++ emulator_src/emulator/Bus.cpp
@@ -0,0 +1,18 @@
+#include "Bus.h"
+
+Bus::Bus()
+{
+}
+
+Bus::~Bus()
+{
+}
+
+void Bus::write(uint16_t addr, uint8_t data)
+{
+}
+
+uint8_t Bus::read(uint16_t addr, bool bReadOnly)
+{
+	return uint8_t();
+}
